ac7ff0aa78412455f729bcfb69990a6c32b54c45
Taha Saif <160765440+Tahasaif3@users.noreply.github.com>
2025-01-29 16:51:42 +0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-     <div className= "h-[88px] bg-customColor"> 
+    <div className="h-[70px] md:h-[80px] lg:h-[88px] bg-customColor w-full flex items-center px-4 md:px-8">
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
