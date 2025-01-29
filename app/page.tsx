9c9b1e740656646905041d4e8a87e2fa6f3f80fe
Taha Saif <160765440+Tahasaif3@users.noreply.github.com>
2025-01-29 16:50:15 +0500

    code uploaded

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,41 @@
-import React from 'react';
-import Image from 'next/image';
-import Header from '@/components/Header';
+"use client"
+
+import React from "react"
+import Image from "next/image"
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
-    <div className="h-screen">    
+    <div className="min-h-screen flex flex-col">
       <Header />
-      {/* Parent div container */}
-      <div className="flex h-[80%]">
-        {/* left side of child div container */}
-        <div className="w-1/2 flex flex-col justify-center items-start m-12">
-          {/* heading og website on left */}
-          <h1 className="text-[44px] font-bold">Welcome To Our Website</h1>
-          <p className="text-[32px] w-[571px]">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry&#39;s standard
+      {/* Main content */}
+      <div className="flex-grow flex flex-col md:flex-row items-center justify-center px-4 md:px-12 py-8 md:py-0">
+        {/* Left side content */}
+        <div className="w-full md:w-1/2 flex flex-col justify-center items-center md:items-start space-y-6 mb-8 md:mb-0">
+          {/* Heading */}
+          <h1 className="text-3xl md:text-4xl lg:text-[44px] font-bold text-center md:text-left">
+            Welcome To Our Website
+          </h1>
+          <p className="text-lg md:text-xl lg:text-[32px] max-w-[571px] text-center md:text-left">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry&#39;s standard
           </p>
-          <br />
-          <button className="text-white bg-black text-[24px] px-4 w-[180px] h-[52px]">
-          Contact US
+          <button className="text-white bg-black text-lg md:text-xl lg:text-[24px] px-4 py-2 w-full md:w-[180px] h-[52px]">
+            Contact Us
           </button>
         </div>
-        {/* Right side of child div container */}
-        <div className="w-1/2 flex justify-center items-center">
-          <Image src="/figma_image.jpeg" alt="Figma image" width={502} height={465} />
+        {/* Right side image */}
+        <div className="w-full md:w-1/2 flex justify-center items-center">
+          <Image
+            src="/figma_image.jpeg"
+            alt="Figma image"
+            width={502}
+            height={465}
+            className="w-full max-w-[502px] h-auto"
+          />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
+
